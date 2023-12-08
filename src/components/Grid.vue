05e7e019bd14395c37ee372ca09b6b43b62419da
grittin <grittin1412@gmail.com>
2023-12-08 18:14:48 +0700

    edit

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,10 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      datas: {},
+    };
+  },
+  mounted() {
+    let fetchRes = fetch(
+      "https://demotrade.efintradeplus.com/ExamAPI/examdata"
+    );
+    fetchRes
+      .then((res) => res.json())
+      .then((d) => {
+        this.datas = d;
+      });
+  },
+};
+</script>
 <template>
-    <div class="min-h-screen grid grid-rows-4 grid-flow-col gap-3">
-      <div class="row-span-2 col-span-1 bg-red-200">1 is a flys.</div>
-      <div class="row-span-1 col-span-1 bg-purple-200">3 is not a tree.</div>
-      <div class="row-span-1 col-span-2 bg-yellow-200">Prepare your mop.</div>
-      <div class="row-span-2 bg-green-200">2 is a fish.</div>
-      <div class="row-span-1 bg-blue-200">4 will win three of a kind.</div>
-      <div class="row-span-4 col-span-1 bg-purple-300">5 is a laughing.</div>
-    </div>
-  </template>
+  <div class="min-h-screen grid grid-rows-4 grid-flow-col gap-3">
+    <div class="row-span-2 col-span-1 bg-red-200" >{{datas.data_1}}</div>
+    <div class="row-span-1 col-span-1 bg-purple-200">{{datas.data_3}}</div>
+    <div class="row-span-1 col-span-2 bg-yellow-200">{{datas.data_6}}</div>
+    <div class="row-span-2 bg-green-200">{{datas.data_2}}</div>
+    <div class="row-span-1 bg-blue-200">{{datas.data_4}}</div>
+    <div class="row-span-4 col-span-1 bg-purple-300">{{datas.data_5}}</div>
+  </div>
+</template>
